727bb7036e2bdd0c3bb6d6b83cc09e4435476c8a
Ian Kabil Felix <ian.felix10@gmail.com>
2019-08-30 14:46:24 +1000

    Updated Readme

--- public/index.php
+++ public/index.php
@@ -1,1 +1,10 @@
 echo composer install --optimize-autoloader --no-dev
+
+
+
+/* This is to compress the routes into one cached file, only use if number of routes gets large.
+
+
+echo php artisan route:cache
+
+*/
